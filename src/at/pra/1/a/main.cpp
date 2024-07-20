07f34116b58d9edfe27c2bdbaf5eb0247ea9462e
hanfeng-chai <171837007+hanfeng-chai@users.noreply.github.com>
2024-07-20 22:24:08 +0800

    Initial commit

---
+++ src/at/pra/1/a/main.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://atcoder.jp/contests/practice/tasks/practice_1"
+#include <bits/stdc++.h>
+
+int main() {
+    int a, b, c;
+    std::string s;
+    std::cin >> a >> b >> c >> s;
+    std::cout << a + b + c << " " << s << std::endl;
+}
